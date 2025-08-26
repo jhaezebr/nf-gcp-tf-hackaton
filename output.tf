e54b59f4800f7f3c8f1fbe2567a5427e47cbc05c
jules.haezebrouck <jules.haezebrouck@uzgent.be>
2025-08-26 12:42:56 +0000

    add more outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "bucket_name" {
   value = google_storage_bucket.demo_bucket.name
 }
+
+output "network_name" {
+  value = google_compute_network.vpc_network.name
+}
+
+output "subnetwork_name" {
+  value = google_compute_subnetwork.vpc_subnetwork.name
+}
